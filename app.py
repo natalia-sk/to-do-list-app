3376e312ec0a7aff8a4d9d13c5e559800f7347a2
Natalia Szokaluk-Krawczyk <nataliaszokaluk@gmail.com>
2020-10-14 15:03:58 +0200

    Small changes.

--- app.py
+++ app.py
@@ -8,17 +8,18 @@ try:
 except ImportError:
     print("Make a PostgreSQL configuration in 'local_settings.py' and then try again.")
     postgresql_config = None
-    exit(0)
+    exit(1)
 
 # in 'local_settings.py':
-# postgresql_config= postgresql+psycopg2://<user>>:<password>@localhost/<database_name>'
+# postgresql_config: str = 'postgresql+psycopg2://<user>:<password>@localhost/<database_name>'
+
 app.config['SQLALCHEMY_DATABASE_URI'] = postgresql_config
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
 class ToDoList(db.Model):
-    __tablename__ = 'to_do_list'
+    __tablename__: str = 'to_do_list'
 
     id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String(240))
@@ -27,14 +28,18 @@ class ToDoList(db.Model):
 
 @app.route('/')
 def index():
-    # show all todos
+    """
+    Main page with a list of all tasks
+    """
     todo_list = ToDoList.query.all()
     return render_template('main_page.html', todo_list=todo_list)
 
 
 @app.route('/add', methods=['POST'])
 def add():
-    # add new task
+    """
+    Adding a new task
+    """
     task = request.form.get('task')
     new_todo = ToDoList(task=task, complete=False)
     db.session.add(new_todo)
@@ -44,7 +49,11 @@ def add():
 
 @app.route('/update/<int:todo_id>')
 def update(todo_id):
-    # update task
+    """
+    Updating a task (done/to do)
+    :param todo_id: an id of a task
+    :type todo_id: integer
+    """
     todo = ToDoList.query.filter_by(id=todo_id).first()
     todo.complete = not todo.complete
     db.session.commit()
@@ -53,7 +62,11 @@ def update(todo_id):
 
 @app.route('/delete/<int:todo_id>')
 def delete(todo_id):
-    # delete task
+    """
+    Deleting a task
+    :param todo_id: an id of a task
+    :type todo_id: integer
+    """
     todo = ToDoList.query.filter_by(id=todo_id).first()
     db.session.delete(todo)
     db.session.commit()
@@ -61,5 +74,6 @@ def delete(todo_id):
 
 
 if __name__ == '__main__':
+    # creating tables in your database
     # db.create_all()
     app.run()
